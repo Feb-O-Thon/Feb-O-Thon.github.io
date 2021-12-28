39f9dca377dfeb8af3672e2e194fa07195cbd134
ProttoyChakraborty <pchakraborty2002@gmail.com>
2021-12-29 01:24:38 +0530

    updates in UI,added Timeline and about us section

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,14 @@
 console.log("working!");
 navbar = document.getElementById("navbar-container");
+trigger = document.getElementById("scroll-trigger");
 console.log(navbar);
 document.addEventListener("scroll", () => {
-  console.log("1");
+  if (window.pageYOffset >= trigger.offsetTop) {
+    console.log(1);
+    navbar.style.backgroundColor = "rgb(2,22,30)";
+  }
+  if (window.pageYOffset <= trigger.offsetTop) {
+    console.log(1);
+    navbar.style.background = "None";
+  }
 });
